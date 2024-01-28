343f6fe3c274bc710c8f51d109c0b4db0ba2afe6
perugi <dominik.perusko@gmail.com>
2024-01-28 22:38:20 +0100

    Create the api fetching and data processing functions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import './reset.css';
 import './styles.css';
 
 import Utilities from '@perugi/web-utilities';
+import getWeather from './getWeather';
 
 Utilities.renderGHLogo();
+
+getWeather('ljubljana').then((data) => {
+  console.log(data);
+});
